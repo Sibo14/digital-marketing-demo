cf44bef4e9fe5733f84a3969a7ec690745cab7d1
Sibo14 <sibzmbatha870@gmail.com>
2024-03-20 22:47:33 +0200

    packages

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 
 export default function HomePage() {
-  const header = {
+  const headerVariants = {
     hidden: { y: "100vh" }, // Start from the bottom of the screen
     show: {
       y: 0, // End at the top of the screen
@@ -14,7 +14,7 @@ export default function HomePage() {
       },
     },
   };
-  const getQoute = {
+  const quoteBtnVariants = {
     hidden: { y: "-100vh" }, // Start from the top of the screen
     show: {
       y: 0, // End at the top of the screen
@@ -22,6 +22,7 @@ export default function HomePage() {
         type: "spring",
         stiffness: 30,
         damping: 10,
+        delay: 1,
       },
     },
   };
@@ -35,11 +36,27 @@ export default function HomePage() {
           backgroundPosition: "center",
         }}
       >
-        <motion.div initial="hidden" animate="show" variants={header}>
-          <h1 className="text-4xl font-bold text-white">Digital Marketing</h1>
-        </motion.div>
+        <div>
+          <motion.h1
+            initial="hidden"
+            animate="show"
+            variants={headerVariants}
+            className="text-4xl font-bold text-white"
+            whileHover={{
+              textShadow: "0px 0px 8px #FF7F11",
+            }}
+          >
+            Digital Marketing
+          </motion.h1>
+        </div>
 
-        <motion.div initial="hidden" animate="show" variants={getQoute}>
+        <motion.div
+          initial="hidden"
+          animate="show"
+          variants={quoteBtnVariants}
+          className="m-3"
+          whileHover={{ scale: 1.1 }}
+        >
           <Button size={"lg"} className="bg-primary">
             Get Quote
           </Button>
@@ -50,7 +67,7 @@ export default function HomePage() {
           <h1 className="text-4xl font-bold">About</h1>
         </div>
         <div className="flex w-full  p-3">
-          <p>
+          <p className="justify-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -60,15 +77,21 @@ export default function HomePage() {
           </p>
         </div>
       </div>
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-4xl font-bold">Pricing</h1>
-        {/* Add your content for the Pricing section here */}
+      <div className="flex flex-col justify-center items-center h-screen bg-primary">
+        <h1 className="text-2xl font-bold">Our Services</h1>
+        <h2 className="text-4xl font-bold">Choose your package</h2>
+        <div className="flex justify-between items-center m-6 gap-3">
+          <h1 className="text-4xl font-bold">SEO </h1>
+          <h1 className="text-4xl font-bold">Social Media Marketing</h1>
+          <h1 className="text-4xl font-bold">Web Design</h1>
+          <h1 className="text-4xl font-bold">Mobile Design </h1>
+        </div>
       </div>
       <div className="flex justify-center items-center h-screen">
         <h1 className="text-4xl font-bold">Testimonials</h1>
         {/* Add your content for the Testimonials section here */}
       </div>
-      <footer className="flex justify-center items-center h-screen">
+      <footer className="flex justify-center items-center h-screen bg-primary">
         <h1 className="text-4xl font-bold">Footer</h1>
         {/* Add your content for the Footer section here */}
       </footer>
