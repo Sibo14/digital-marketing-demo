a1c8ceda4c2ce5decb9250dbd05fbf74c70be573
Sibo14 <sibzmbatha870@gmail.com>
2024-04-02 22:02:18 +0200

    Testimonial
    Testimonial

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -1,17 +1,34 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { motion } from "framer-motion";
+import { Button } from "@/components/ui/Button";
+import { delay, motion, useAnimation, useInView } from "framer-motion";
 import Image from "next/image";
 import Lottie from "lottie-react";
 import animationData from "../images/lottifilesanimation.json";
+import aboutLottie from "../images/lottifilesAbout.json";
+import { useEffect, useRef } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/Carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import profilepic from "../images/profilepic.jpg";
 
 interface Package {
   name: string;
   description: string;
   img: string;
 }
+interface Testimonial {
+  name: string;
+  review: string;
+}
 
 export default function HomePage() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   const packages: Package[] = [
     {
       name: "SEO",
@@ -32,6 +49,24 @@ export default function HomePage() {
       name: "Mobile",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       img: require("../images/mobile.png"),
+    },
+  ];
+  const testimonials: Testimonial[] = [
+    {
+      name: "John Doe",
+      review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "Jane Doe",
+      review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "John Doe",
+      review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "Jane Doe",
+      review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
   ];
   const headerVariants = {
@@ -69,6 +104,24 @@ export default function HomePage() {
       },
     },
   };
+  const aboutVariants = {
+    hidden: { opacity: 0, y: 75 }, // Start from the top of the screen
+    show: {
+      type: "spring",
+      stiffness: 30,
+      damping: 10,
+      opacity: 1,
+      y: 0,
+      delay: 5.5,
+    },
+  };
+
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("show");
+    }
+  }, [isInView, mainControls]);
 
   const ServicePack = ({ name, description, img }: Package) => (
     <motion.div
@@ -134,7 +187,7 @@ export default function HomePage() {
       <div className="flex flex-col justify-center items-center h-screen ">
         <h1 className="text-2xl font-bold">Our Services</h1>
         <h2 className="text-4xl font-bold my-10">Choose your package</h2>
-        <div className="flex justify-start items-center m-6 gap-3 h-full w-full p-4">
+        <div className="flex justify-start items-center m-6 gap-3 h-full w-full p-4 overflow-hidden">
           {/* <h1 className="text-4xl font-bold">SEO </h1>
           <h1 className="text-4xl font-bold">Social Media Marketing</h1>
           <h1 className="text-4xl font-bold">Web Design</h1>
@@ -150,29 +203,124 @@ export default function HomePage() {
           ))}
         </div>
       </div>
-      <div className="flex justify-center items-center h-screen bg-secondary">
-        <div className="flex h-full w-full  items-center justify-end p-3">
-          <h1 className="text-4xl font-bold">About</h1>
-        </div>
-        <div className="flex w-full  p-3">
-          <p className="justify-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
-          </p>
+      <div className="flex flex-col justify-center items-center h-screen bg-secondary">
+        <h1 className="text-4xl font-bold text-white">About US </h1>
+        <div className="flex items-center h-full">
+          <motion.div
+            className="flex flex-col h-full w-full  items-center justify-center p-3 "
+            initial="hidden"
+            animate={mainControls}
+            variants={aboutVariants}
+            ref={ref}
+          >
+            <Lottie animationData={aboutLottie} className="h-96" />
+          </motion.div>
+          <motion.div
+            className="flex w-full  p-3"
+            initial="hidden"
+            animate={mainControls}
+            variants={aboutVariants}
+            ref={ref}
+          >
+            <p className="justify-center text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </p>
+          </motion.div>
         </div>
       </div>
 
-      <div className="flex justify-center items-center h-screen ">
+      <div className="flex flex-col justify-center items-center h-screen ">
         <h1 className="text-4xl font-bold">Testimonials</h1>
-        {/* Add your content for the Testimonials section here */}
+        <div className="flex h-full items-center justify-center w-full">
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className="w-full max-w-4xl "
+          >
+            <CarouselContent>
+              {testimonials.map((item, index) => (
+                <CarouselItem
+                  key={index}
+                  className="md:basis-1/2 lg:basis-1/3 "
+                >
+                  <div className="p-1">
+                    <Card className=" bg-white rounded-lg shadow-md overflow-hidden h-96 w-60 ">
+                      <CardContent className="flex flex-col aspect-square items-center justify-center p-6 space-y-4 ">
+                        <Image
+                          src={profilepic}
+                          alt="/"
+                          objectFit="cover"
+                          width={100}
+                          height={100}
+                          className="rounded-full"
+                        />
+
+                        <h1 className="text-xl font-bold">{item.name}</h1>
+                        <p className="text-base text-gray-600">{item.review}</p>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
       </div>
-      <footer className="flex justify-center items-center h-screen bg-secondary ">
-        <h1 className="text-4xl font-bold">Footer</h1>
-        {/* Add your content for the Footer section here */}
+      <footer className="flex flex-col justify-center items-end p-4 bg-secondary text-white ">
+        <div className="container mx-auto flex flex-wrap justify-between items-center">
+          <div>
+            <h1 className="font-bold text-2xl mb-2">Company Name</h1>
+            <p>1234 Street Name</p>
+            <p>City, State, Zip</p>
+          </div>
+          <div>
+            <h2 className="font-bold text-xl mb-2">Links</h2>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-bold text-xl mb-2">Follow Us</h2>
+            <ul>
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-full flex justify-center my-4">
+          <p>
+            &copy; {new Date().getFullYear()} Company Name. All rights reserved.
+          </p>
+        </div>
       </footer>
     </div>
   );
